cc400350153d4bf46a3ea195e54be67be40fac64
Shoxrux <shomurodovshoxrux1998@gmail.com>
2022-06-30 16:58:40 +0300

    fixEmptyingArray

--- index.js
+++ index.js
@@ -19,20 +19,76 @@ console.log(numbers);
 
 //✌✌✌ Finding Elements (Primitives) ✌✌✌
 const nums = [1, 2, 3, 1, 4];
-console.log(nums.indexOf("1"));
+console.log(nums.indexOf(2));
 console.log(nums.lastIndexOf(1));
 console.log(nums.indexOf(1) !== -1);
 console.log(nums.includes(1));
-console.log("first");
-//✌✌✌ Finding Elements (Reference Types) ✌✌✌
 
-//🏋️‍♂️🏋️‍♂️🏋️‍♂️ Prime Numbers🏋️‍♂️🏋️‍♂️🏋️‍♂️
-function findOccurrences(array, searchElement) {
-  let occurence = 0;
-  array.reduce((acc, curr) => {
-    occurence = curr === searchElement ? 1 : 0;
-    return (occurence = +curr);
-  }, 0);
-  return occurence;
+//✌✌✌ Finding Elements (Reference Types) ✌✌✌
+const courses = [
+  { id: 1, name: "Javascript" },
+  { id: 2, name: "Node" },
+  { id: 3, name: "React" },
+];
+// const course = courses.find(function (course) {
+//   return course.name === "Javascript";
+// });
+// const courseIndex = courses.findIndex(function (course) {
+//   return course.name === "Node";
+// });
+// console.log(course); //{id: 1, name: 'Javascript'}
+// console.log(courseIndex); //1
+
+//✌✌✌ Arrow Functions ✌✌✌
+
+const course = courses.find((course) => {
+  return course.name === "Javascript";
+});
+const courseIndex = courses.findIndex((course) => {
+  return course.name === "Node";
+});
+console.log(course); //{id: 1, name: 'Javascript'}
+console.log(courseIndex); //1
+
+//✌✌✌ Removing Elements ✌✌✌
+const numbersRemoving = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//Adding elements
+numbersRemoving.push(); //last
+numbersRemoving.unshift(); //beginning
+numbersRemoving.slice(); //somewhere
+
+//Removing elements
+numbersRemoving.pop(); //last
+console.log(numbersRemoving); // [1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+numbersRemoving.shift(); //beginning
+console.log(numbersRemoving); // [ 2, 3, 4, 5, 6, 7, 8, 9 ]
+numbersRemoving.splice(2, 1); //somewhere
+console.log(numbersRemoving); // [ 2, 3, 5, 6, 7, 8, 9 ]
+
+//✌✌✌ Emptying an Array ✌✌✌
+
+let numbersEmpty = [1, 2, 3, 4, 5, 6, 7, 8];
+let anotherNumbers = numbersEmpty;
+
+// Solution 1
+// numbersEmpty = [];
+// console.log(numbersEmpty); // []
+// console.log(anotherNumbers); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+// Solution 2
+// numbersEmpty.length = 0;
+// console.log(numbersEmpty); // []
+// console.log(anotherNumbers); // []
+
+// Solution 3
+// numbersEmpty.splice(0, numbersEmpty.length);
+// console.log(numbersEmpty); //[]
+console.log(anotherNumbers); //[1, 2, 3, 4, 5, 6, 7, 8]
+
+// Solution 4
+while (numbersEmpty.length > 0) {
+  numbersEmpty.pop();
 }
-console.log(findOccurrences([1, 2, 3, 1, 1, 4], 1));
+console.log(numbersEmpty); // []
+console.log(anotherNumbers); // []
+//🏋️‍♂️🏋️‍♂️🏋️‍♂️ Prime Numbers🏋️‍♂️🏋️‍♂️🏋️‍♂️
